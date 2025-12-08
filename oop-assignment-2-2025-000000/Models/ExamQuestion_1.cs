f202a474a353f772a7538bbf7619bfdb2002136f
JohnRowleyDorsetCollege <john.rowley@dorset.ie>
2025-12-08 21:17:49 +0000

    Add project files.

---
+++ oop-assignment-2-2025-000000/Models/ExamQuestion_1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace oop_assignment_2_2025_000000.Models
+{
+    public static class ExamQuestion_1
+    {
+        public static void Run()
+        {
+            Console.WriteLine("Exam Question 1 executed.");
+            SampleQuestionAMethod();
+            SampleQuestionBMethod();
+        }
+        public static void SampleQuestionAMethod()
+        {
+            // Sample method implementation
+            Console.WriteLine("SampleMethod");
+        }
+
+        public static void SampleQuestionBMethod()
+        {
+            // Sample method implementation
+            Console.WriteLine("SampleMethod");
+        }
+    }
+}
---
+++ oop-assignment-2-2025-000000/Models/ExamQuestion_2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace oop_assignment_2_2025_000000.Models
+{
+    public static class ExamQuestion_2
+    {
+        public static void Run()
+        {
+            Console.WriteLine("Exam Question 2 executed.");
+            SampleQuestionMethod();
+        }
+        public static void SampleQuestionMethod()
+        {
+            // Sample method implementation
+            Console.WriteLine("SampleMethod");
+        }
+    }
+}
